de129200f9e269432b9450d32dea15d57089ed43
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-18 20:46:02 +0000

    Created card class and constructor

---
+++ Card.java
@@ -0,0 +1,15 @@
+package CardGame;
+
+public class Card{
+
+  private String suit;
+  private int value;
+  private String rank;
+
+  public Card(String suit, int value, String rank) {
+    this.suit = suit;
+    this.value = value;
+    this.rank = rank;
+  }
+
+  
