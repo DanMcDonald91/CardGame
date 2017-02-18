cb2e4b13f288985597e2fec5f5bd73bff8298ae8
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-18 21:09:07 +0000

    Created deck class and constructor

---
+++ Deck.java
@@ -0,0 +1,15 @@
+package CardGame;
+import java.util*;
+
+
+public class Deck{
+
+  private ArrayList<Card> cardDeck;
+
+  public Deck() {
+    this.cardDeck = new ArrayList(Card)();
+  }
+
+  // Deck Methods
+
+}
